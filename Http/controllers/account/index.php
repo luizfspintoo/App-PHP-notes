c6d47eac05dfc6b1cdd9d4f172c650514d7fb9f8
luizfspintoo <luizfspintoo@gmail.com>
2024-03-13 22:41:30 -0300

    configurando a conta

--- Http/controllers/account/index.php
+++ Http/controllers/account/index.php
@@ -5,7 +5,7 @@ use Core\Database;
 
 $db = App::resolve(Database::class);
 
-$userAccount = $db->query("SELECT * FROM users WHERE id = 23")->get();
+$userAccount = $db->query("SELECT * FROM users WHERE id = 1")->get();
 
 view("account/account.view.php", [
     "userAccount" => $userAccount[0]
