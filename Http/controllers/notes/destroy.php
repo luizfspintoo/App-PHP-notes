3354f5e4528c17c032c9552f5010fe95691a6a3f
luizfspintoo <luizfspintoo@gmail.com>
2024-03-19 09:41:45 -0300

    session usuário

--- Http/controllers/notes/destroy.php
+++ Http/controllers/notes/destroy.php
@@ -4,7 +4,6 @@ use Core\App;
 use Core\Database;
 
 $db = App::resolve(Database::class);
-
 $user_id = intval($_SESSION["user"]["id"]);
 
 $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_POST["id"]])->findOrFail();
