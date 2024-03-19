6622ad051da365bdbe28b0d682ff0cb875a2dcb6
luizfspintoo <luizfspintoo@gmail.com>
2024-03-19 10:00:08 -0300

    configurando login

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -3,6 +3,10 @@
 use Core\Authenticator;
 use Core\Session;
 use Http\Form\LoginForm;
+use Core\Database;
+use Core\App;
+
+$db = App::resolve(Database::class);
 
 $email = $_POST["email"];
 $password = $_POST["password"];
