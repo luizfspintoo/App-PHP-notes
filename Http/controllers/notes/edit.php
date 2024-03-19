7c8864efca28503c78df6455767b9c0d69a77397
luizfspintoo <luizfspintoo@gmail.com>
2024-03-19 10:01:00 -0300

    configurando login

--- Http/controllers/notes/edit.php
+++ Http/controllers/notes/edit.php
@@ -5,10 +5,6 @@ use Core\Database;
 use Core\Validator;
 
 $db = App::resolve(Database::class);
-<<<<<<< HEAD
-
-=======
->>>>>>> 961ec5ae3316ee6c1aede1a9caded7d4278ed589
 $id = $_GET["id"];
 
 $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $id])->findOrFail();
