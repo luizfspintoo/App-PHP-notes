32f75fe164b6c6f99af28053c0e064aad389769c
Nathan <nathanpereirasouza@gmail.com>
2024-05-01 21:01:58 -0300

    cria um container para o email provider

--- bootstrap.php
+++ bootstrap.php
@@ -3,11 +3,16 @@
 use Core\App;
 use Core\Container;
 use Core\Database;
+use Core\EmailProvider;
 
 $container = new Container();
 
 $container->bind("Core\Database", function () {
     return new Database();
+});
+
+$container->bind("Core\EmailProvider", function () {
+    return new EmailProvider();
 });
 
 App::setContainer($container);
